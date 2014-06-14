665e5d3650cd4cf55fd1f9a4eaac98256d637668
Tom Moertel <tom@moertel.com>
2014-06-14 17:34:02 -0400

    Factor out common Makefile logic

--- Makefile
+++ Makefile
@@ -1,24 +1,2 @@
-gtest_flags = $$(gtest-config --cppflags --cxxflags --ldflags --libs)
-test_flags  = $(gtest_flags) -fprofile-arcs -ftest-coverage -lgcov
-
-all: test
-.PHONY: all
-
-.PHONY: test
-test: consumers_and_producers_test
-	./$<
-
-.PHONY: coverage
-coverage: consumers_and_producers_test
-	mkdir -p coverage
-	lcov --directory . --zerocounters
-	./$<
-	lcov --directory . --capture --output-file coverage/test.info
-	genhtml -o coverage coverage/test.info
-	@echo "### Coverage report is in file://$$(pwd)/coverage/index.html"
-
-%: %.cc
-	g++ --std=c++11 $(test_flags) -o $@ $?
-
-clean:
-	rm -rf consumers_and_producers_test coverage *.gc{da,no}
+tests = consumers_and_producers_test
+include rules.mk
---
+++ rules.mk
@@ -0,0 +1,36 @@
+# Makefile rules for building and testing basic C++ code.
+# To use the rules, create a Makefile defining your objects, tests,
+# and any special link flags you need, and then include the rules:
+#   objects = name_of_library.o...
+#   tests = name_of_binary_test name_of_another_binary_test...
+#   link_flags = -lprotobuf
+#   include rules.mk
+
+gtest_flags = $$(gtest-config --cppflags --cxxflags --ldflags --libs)
+test_flags  = $(gtest_flags) -fprofile-arcs -ftest-coverage
+link_flags := $(link_flags) -lgcov
+
+test: $(tests)
+	for t in $?; do echo "### Running $t ###"; ./$$t; done
+.PHONY: test
+
+$(tests): $(objects)
+
+.PHONY: coverage
+coverage: $(tests)
+	mkdir -p coverage
+	lcov --directory . --zerocounters
+	for t in $?; do echo "### Running $$t ###"; ./$$t; done
+	lcov --directory . --capture --output-file coverage/test.info
+	genhtml -o coverage coverage/test.info
+	@echo "### Coverage report is in file://$$(pwd)/coverage/index.html"
+
+.PHONY: clean
+clean:
+	rm -rf $(tests) coverage *.gc{da,no}
+
+%: %.cc
+	g++ --std=c++11 $(test_flags) $(link_flags) -o $@ $< $(objects)
+
+%.o: %.cc
+	g++ --std=c++11 $(test_flags) -c -o $@ $?
