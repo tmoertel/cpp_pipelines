817de89dc7cf47001844d4d0adf167a2085299b6
Tom Moertel <tom@moertel.com>
2014-06-15 10:49:02 -0400

    Improve tests for protobuf accessors

--- examples/protobuf-traversal/proto_accessors_test.cc
+++ examples/protobuf-traversal/proto_accessors_test.cc
@@ -1,10 +1,18 @@
 // Tests for uniform interface for protocol buffers.
 // Tom Moertel <tom@moertel.com>
 
+
+#include <set>
+#include <string>
+#include <vector>
+
 #include "../../consumers_and_producers.h"
 #include "example.pb.h"
-
 #include "gtest/gtest.h"
+
+using std::set;
+using std::string;
+using std::vector;
 
 namespace example {
 
@@ -30,11 +38,6 @@ Producer<const T*> Produce(const google::protobuf::RepeatedPtrField<T>& ts) {
   };
 }
 
-// Produce the name of a person if the person has one.
-static Filter<const Person*, const std::string*> PersonName{
-    [](const Person* p) { return MUnit(&p->name()); }
-};
-
 // Produce the teams within a company.
 static Filter<const Company*, const Team*> CompanyTeams{
   [](const Company* p) { return Produce(p->teams()); }
@@ -53,10 +56,15 @@ static Filter<const Team*, const Person*> TeamMembers{
 };
 
 // Produce the name of a team if it has one.
-static Filter<const Team*, const std::string*> TeamName{
+static Filter<const Team*, const string*> TeamName{
   [](const Team* p) {
-    return p->has_name() ? MUnit(&p->name()) : PZero<const std::string*>();
+    return p->has_name() ? MUnit(&p->name()) : PZero<const string*>();
   }
+};
+
+// Produce the name of a person.
+static Filter<const Person*, const string*> PersonName{
+  [](const Person* p) { return MUnit(&p->name()); }
 };
 
 
@@ -86,29 +94,59 @@ TEST(ProducersAndConsumers, Basics) {
 
   {
     // Our test consumer will record all of the names it is given.
-    std::vector<std::string> names;
-    Consumer<const std::string*> add_to_names {
-      [&](const std::string* name) { names.push_back(*name); }
+    vector<string> names;
+    Consumer<const string*> add_to_names {
+      [&](const string* name) { names.push_back(*name); }
     };
 
     // We can find all team names.
     (CompanyTeams * TeamName)(&company)(add_to_names);
-    EXPECT_EQ(std::vector<std::string>({"The Three Stooges",
-                                        "The X-Men Lite"}),
+    EXPECT_EQ(vector<string>({"The Three Stooges", "The X-Men Lite"}),
               names);
 
     // We can find the names of all members of all teams.
     names.clear();
     (CompanyTeams * TeamMembers * PersonName)(&company)(add_to_names);
-    EXPECT_EQ(std::vector<std::string>({"Curly", "Larry", "Moe",
-                                        "Colossus", "Wolverine",
-                                        "Lone Wolf McQuade"}),
+    EXPECT_EQ(vector<string>({"Curly", "Larry", "Moe",
+                              "Colossus", "Wolverine",
+                              "Lone Wolf McQuade"}),
               names);
 
     // We can find the names of all managers, too.
     names.clear();
     (CompanyTeams * TeamManager * PersonName)(&company)(add_to_names);
-    EXPECT_EQ(std::vector<std::string>({"Prof. X"}), names);
+    EXPECT_EQ(vector<string>({"Prof. X"}), names);
+
+    // Yes, we can find the names of all managers *and* team members.
+    names.clear();
+    auto filter = CompanyTeams * (TeamManager + TeamMembers) * PersonName;
+    filter(&company)(add_to_names);
+    EXPECT_EQ(vector<string>({"Curly", "Larry", "Moe",
+                              "Prof. X",  // Manager.
+                              "Colossus", "Wolverine",
+                              "Lone Wolf McQuade"}),
+              names);
+
+    // As you might expect, * is right-distributive over +. Therefore,
+    // the following filter must produce the same results as the previous.
+    vector<string> previous_names = names;  // Expect same as last time.
+    filter = CompanyTeams * (TeamManager * PersonName +
+                             TeamMembers * PersonName);
+    names.clear();
+    filter(&company)(add_to_names);
+    EXPECT_EQ(previous_names, names);
+
+    // But * is not left-distributive over +: output order may change.
+    filter = (CompanyTeams * TeamManager +  // Want all team managers first.
+              CompanyTeams * TeamMembers    // And then all team members.
+             ) * PersonName;
+    names.clear();
+    filter(&company)(add_to_names);
+    EXPECT_EQ(vector<string>({"Prof. X",  // Manager.
+                              "Curly", "Larry", "Moe",
+                              "Colossus", "Wolverine",
+                              "Lone Wolf McQuade"}),
+              names);
   }
 }
 
